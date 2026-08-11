033dda7e55a3ad88c2eb49d925d32f3e95e91b3b
cybfong <bosco.fong@gmail.com>
2026-08-11 23:01:06 +0900

    Completed database tables design, data insertion, and model changes (add UserCredential.java)

--- src/main/java/com/oji/mini_crm_server/model/UserCredential.java
+++ src/main/java/com/oji/mini_crm_server/model/UserCredential.java
@@ -3,10 +3,12 @@ package com.oji.mini_crm_server.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Data
+@ToString(exclude = "passwordHash")
 @NoArgsConstructor
 @Entity
 @Table(name = "user_credentials")
@@ -20,7 +22,10 @@ public class UserCredential {
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @OneToOne(fetch = FetchType.LAZY)
